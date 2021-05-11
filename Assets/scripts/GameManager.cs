024093920011c5e4f055e08fe0fbb334a6558366
GeunTae KIM <k031900@gmail.com>
2021-05-12 00:06:36 +0900

    옮기기 완료

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject talkPanel;
+    public Text talkText;
+    public GameObject scannedObject;
+    public GameObject menuPanel;
+    public GameObject Player;
+    public bool isScan;
+
+    private void Start()
+    {
+        GameLoad();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (menuPanel.activeSelf == true)
+            {
+                menuPanel.SetActive(false);
+            }
+            else
+            {
+                menuPanel.SetActive(true);
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    public void Scan(GameObject scanObj)
+    {
+        if (isScan == true)
+        {
+            isScan = false;
+            talkPanel.SetActive(false);
+        }
+        else
+        {
+            isScan = true;
+
+            //player가 스캔한 오브젝트를 받음
+            scannedObject = scanObj;
+
+            //판넬 활성화
+            talkPanel.SetActive(true);
+
+            //public을 통해 선택한 text를 위의 오브젝트의 이름 값을 가져와서 출력
+            talkText.text = "이건 " + scanObj.name + "이다.";
+
+        }
+
+    }
+
+    public void GameSave()
+    {
+        //playerPrefs로 저장할 변수를 선택하고 새로운 변수명 지정
+        PlayerPrefs.SetFloat("PlayerX", Player.transform.position.x);
+        PlayerPrefs.SetFloat("PlayerY", Player.transform.position.y);
+
+        //playerPrefs의 세이브 함수 실행
+        PlayerPrefs.Save();
+
+        menuPanel.SetActive(false);
+
+        Debug.Log("저장완료");
+    }
+
+    public void GameLoad()
+    {
+        if (!PlayerPrefs.HasKey("PlayerX"))
+        {
+            return;
+        }
+
+        //저장된 데이터를 불러와서 새로운 변수로 지정
+        float x = PlayerPrefs.GetFloat("PlayerX");
+        float y = PlayerPrefs.GetFloat("PlayerY");
+
+        //데이터를 바탕으로 플레이어의 위치를 지정
+        Player.transform.position = new Vector3(x, y, 0);
+
+        menuPanel.SetActive(false);
+
+        Debug.Log("불러오기 완료");
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/scripts/PlayerAction.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+
+    float v;
+    float h;
+    public float speed;
+    public GameManager manager;
+    Vector3 dirVec;
+    GameObject ScanObject;
+
+    Rigidbody2D rigid;
+    Animator anim;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //매니저에서 대화 처리를 하고 있을때는 이동불가
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        //h = manager.isScan ? 0 : Input.GetAxisRaw("Horizontal");
+        //v = manager.isScan ? 0 : Input.GetAxisRaw("Vertical");
+
+        if (h != 0 || v != 0)
+        {
+            anim.SetFloat("hAxisRaw", h);
+            anim.SetFloat("vAxisRaw", v);
+            anim.SetBool("IsWalk", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalk", false);
+        }
+
+        //보는 방향값 얻기
+        if (anim.GetFloat("vAxisRaw") == 1)
+        {
+            dirVec = Vector3.up;
+            //print("0,1");
+        }else if (anim.GetFloat("vAxisRaw") == -1)
+        {
+            dirVec = Vector3.down;
+            //print("0,-1");
+        }
+        else if (anim.GetFloat("hAxisRaw") == -1)
+        {
+            dirVec = Vector3.left;
+            //print("-1,0");
+        }
+        else if (anim.GetFloat("hAxisRaw") == 1)
+        {
+            dirVec = Vector3.right;
+            //print("1,0");
+        }
+
+        //스캔 활성화
+        if (Input.GetButtonDown("Jump") && ScanObject != null)
+        {
+            //매니저의 액션 함수로 스캔한 오브젝트를 전달, 이후 매니저에서 UI로 텍스트 넘겨줌
+            manager.Scan(ScanObject);
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v) * speed;
+
+        //Ray 보여주기
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+
+        //스캔할 레이어 선택
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("ScanableObject"));
+
+        //스캔해서 ScanObject에 스캔된 오브젝트를 저장
+        if (rayHit.collider != null)
+        {
+            ScanObject = rayHit.collider.gameObject;
+        }
+        else
+        {
+            ScanObject = null;
+        }
+    }
+}
