302063ef7a46be4195e1df3b3f00ff303d434ba5
GeunTae KIM <k031900@gmail.com>
2021-05-11 21:40:33 +0900

    1

---
+++ Assets/Tiles/Sprite-0001.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Sprite-0001" tilewidth="32" tileheight="32" tilecount="910" columns="26">
+ <image source="source/Sprite-0001.png" width="850" height="1134"/>
+</tileset>
