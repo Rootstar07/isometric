744cf3f6cb4d215451e7312d12e47b3432255e12
GeunTae KIM <k031900@gmail.com>
2021-05-26 19:18:50 +0900

    1

---
+++ Assets/Scripts/GM_for_sky.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using BayatGames.SaveGameFree;
+
+public class GM_for_sky : MonoBehaviour
+{
+    public Animator talkPanel;
+    public Text talkText;
+    public GameObject scannedObject;
+    public GameObject menuPanel;
+    public GameObject Player;
+    public Image PortraitImg;
+    public Animator protraitAni;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
