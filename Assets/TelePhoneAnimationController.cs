5bcd440d16530c2a2e5b954aa5e3f6cbd8d6f742
GeunTae KIM <k031900@gmail.com>
2021-06-04 13:52:59 +0900

    핸드폰 업데이트

---
+++ Assets/TelePhoneAnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelePhoneAnimationController : MonoBehaviour
+{
+    public Animator myAnimator;
+
+    public void ToggleBool(string name)
+    {
+        //myAnimator.SetBool(name, !myAnimator.GetBool(name));
+
+        if (myAnimator.GetBool("isTouch") == true && myAnimator.GetBool("isTextUp") == true)
+            //둘다 열린상태에서 닫으려고 하면 텍스트만 닫히게
+            myAnimator.SetBool("isTextUp", false);
+        else if(myAnimator.GetBool("isTouch") == true && myAnimator.GetBool("isNewsUp") == true)
+            myAnimator.SetBool("isNewsUp", false);
+        else
+            myAnimator.SetBool(name, !myAnimator.GetBool(name));
+
+    }
+}
