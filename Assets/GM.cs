2765bbaae0e352fedd3f370e4dece6cdba4671c4
GeunTae KIM <k031900@gmail.com>
2021-06-05 14:46:13 +0900

    1

---
+++ Assets/GM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class GM : MonoBehaviour
+{
+    public Flowchart flowchart;
+
+    public void Test()
+    {
+        float rate = flowchart.GetFloatVariable("Rate");
+        Debug.Log(rate);
+    }
+}
---
+++ Assets/MailViewer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MailViewer : MonoBehaviour
+{
+    public GameObject message;
+    public Text title1;
+    public Text title2;
+    public Text sender;
+    public Text content1;
+    public Text content2;
+    public Text mailtitle;
+    public Text mailsender;
+
+    [TextArea]
+    public string 내용;
+
+    public void ToggleMessage()
+    {
+
+        message.SetActive(true);
+        title1.text = title2.text = mailtitle.text;
+        sender.text = mailsender.text;
+        content1.text = content2.text = 내용;
+   
+    }
+}
