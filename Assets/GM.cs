996153327569b128d1e9a2d255dbc3e1404648a6
GeunTae KIM <k031900@gmail.com>
2021-06-05 18:09:28 +0900

    1

--- Assets/GM.cs
+++ Assets/GM.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
+using UnityEngine.UI;
 
 public class GM : MonoBehaviour
 {
     public Flowchart flowchart;
+    public Text _rate;
+    public Text finalEarn;
+    public MenuButton[] menubutton;
+    public GameObject MenuNext;
+    public Animator telephoneAnimator;
+    public int 현재선택된메뉴코드;
 
-    public void Test()
+    public void DayEnd()
     {
         float rate = flowchart.GetFloatVariable("Rate");
         Debug.Log(rate);
+
+        _rate.text = rate.ToString();
+        finalEarn.text = (rate * 6700).ToString();
+
+        foreach (var i in menubutton)
+        {
+           i.SetMenu(rate);
+        }
+        
+    }
+
+    public void MenuClick(int code)
+    {
+        현재선택된메뉴코드 = code;
+        MenuNext.GetComponent<Button>().interactable = true;
+        MenuNext.GetComponentInChildren<Text>().text = code.ToString();
+    }
+
+    public void EndReview()
+    {
+        telephoneAnimator.SetBool("isTouch", true);
+        telephoneAnimator.SetBool("EndOfTheDay", false);
+        telephoneAnimator.SetBool("ChooseFood", false);
+        telephoneAnimator.SetBool("TodayReview", false);
     }
 }
---
+++ Assets/MenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    public float foodPrice;
+
+    public void SetMenu(float rate)
+    {
+        if (rate * 6700 < foodPrice)
+        {
+            gameObject.GetComponent<Button>().interactable = false;
+        }
+    }
+
+}
