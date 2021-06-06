807748ee0a8b7fa63c739998b62b85d4908dd7a5
GeunTae KIM <k031900@gmail.com>
2021-06-06 19:05:45 +0900

    사진추가

---
+++ Assets/DialogController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogController : MonoBehaviour
+{
+    public Animator dialogAnimator;
+    public GameObject 조건;
+
+    public void Update()
+    {
+        if (조건.activeSelf == false)
+        {
+            dialogAnimator.SetBool("isMenu", true);
+        }else
+        {
+            dialogAnimator.SetBool("isMenu", false);
+        }
+    }
+
+
+}
